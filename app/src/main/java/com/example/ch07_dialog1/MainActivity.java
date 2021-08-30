6ff89361ec29cd6aa4c8c9c10724fd083a95b483
KJB <drv98@naver.com>
2021-08-30 11:18:21 +0900

    7장 다이얼로그 누가,오레오

---
+++ app/src/main/java/com/example/ch07_dialog1/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.ch07_dialog1;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final Button button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+
+            final boolean[] checkArray = new boolean[] { false, false, false };
+
+            public void onClick(View v)
+            {
+                final String[] versionArray = new String[] { "누가", "오레오", "파이" };
+//                final boolean[] checkArray = new boolean[] { true, false, false };
+                AlertDialog.Builder dlg = new AlertDialog.Builder(MainActivity.this);
+                dlg.setTitle("좋아하는 버전은?");
+                dlg.setIcon(R.mipmap.ic_launcher);
+
+
+                dlg.setMultiChoiceItems(versionArray, checkArray,
+                        new DialogInterface.OnMultiChoiceClickListener() {
+
+                            public void onClick(DialogInterface dialog,
+                                                int which, boolean isChecked) {
+
+                                String str1="";
+                                int cnt=0;
+                                for(int i=0;i<versionArray.length;++i)
+                                {
+//                                    if(checkArray[i]==true)
+                                    if(checkArray[i])
+                                    {
+                                        if(cnt==0)
+                                            str1 += versionArray[i];
+                                        else
+                                            str1 += "," +versionArray[i];
+
+                                        cnt++;
+                                    }
+                                }
+
+                                button1.setText(str1);
+                                //button1.setText(versionArray[which]);
+                            }
+                        });
+                dlg.setPositiveButton("닫기", null);
+                dlg.show();
+
+            }
+        });
+    }
+}
